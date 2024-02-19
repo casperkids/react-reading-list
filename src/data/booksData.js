8772709619cc0d47f49d3e79d81486b1da969c66
casperkids <yukishiroi@protonmail.com>
2024-02-19 20:48:11 +0100

    adding BookCard component first ver reading list

---
+++ src/data/booksData.js
@@ -0,0 +1,51 @@
+export let books = [
+    {
+     title : 'Momo',
+     authors : 'Michael Ende',
+     year : '1973',
+     description: 'Momo, also known as The Grey Gentlemen or The Men in Grey, is a fantasy novel',
+     coverImage: 'bookcovers/MOMO.jpg',
+     memo: 'cassiopeia, p3',
+     haveRead: true,
+     favorite: true
+    },
+    {
+    title : "Don't Sleep, There Are Snakes",
+    authors : 'Daniel L. Everett',
+    year : '2008',
+    description: 'Part passionate memoir, part scientific exploration, a life-changing tale set among a small tribe of Amazonian Indians in Brazil that offers a riveting look into the nature of language, thought, and life itself.',
+    coverImage: 'bookcovers/Default_book copy.jpg',
+    memo: 'Life and Language in the Amazonian Jungle',
+    haveRead: false,
+    favorite:true
+    },
+    {
+    title : 'Lobo, the King of Currumpaw',
+    authors : 'Ernest Thompson Seton',
+    year : '1898',
+    description: 'Based on the non-fiction account by Seton, Lobo is an 1890s wolf from an account by Seton who was a naturalist, and was a bounty hunter in the real-life story. Filming took place in Sedona, Arizona.',
+    coverImage: 'bookcovers/Default_book copy.jpg',
+    favorite:true,
+    haveRead: false,
+    memo: 'wolf'
+  
+    },
+    {
+    title : 'About Face',
+    authors : 'Alan Cooper, Robert Reimann',
+    year : '2014',
+    description: 'The Essentials of Interaction Design ',
+    coverImage: 'bookcovers/Default_book copy.jpg',
+    memo: 'The Essentials of Interaction Design'
+    },
+    {
+    title : 'Wilder Mann',
+    authors : 'Charles Fréger',
+    year : '2012',
+    description: 'The transformation of man into beast is a central aspect of traditional pagan rituals that are centuries old and which celebrate the seasonal cycle, fertility, life and death.',
+    coverImage: 'bookcovers/Default_book copy.jpg',
+    favorite:true,
+    haveRead: true,
+    memo: ''
+    }
+  ]
