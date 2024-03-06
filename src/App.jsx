a2f62874b25620df17efae298c51f6b84a8a77df
casperkids <yukishiroi@protonmail.com>
2024-03-06 15:15:18 +0100

    layout/favorite  fiexd

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useEffect, useState } from 'react'
 import { books as booksData} from "./data/booksData.js"
 import BookCard from './components/BookCard.jsx'
-// import { apiBooks as apiBooksData} from './components/ApiBookCard.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBookOpenReader, faBookmark, faHeart } from '@fortawesome/free-solid-svg-icons'; 
+import { faBookOpenReader, faBookmark, faHeart,faArrowUp } from '@fortawesome/free-solid-svg-icons'; 
 
 
 function App() {
 
   const [books, setBooks] = useState(booksData);
   const [apiBooksData, setApiBooksData] = useState([]);
-  // const [haveReadSum, setHaveReadSum] = useState(0)
-  // const [favSum, setFavSum] = useState(0)
-
+  const [favSum, setFavSum] = useState(0)
+  const [readSum, setReadSum] = useState(0)
 
   //add function to change state of book(haveRead: true or false)
   const toggleReadStatus = (id) => {
@@ -24,18 +22,16 @@ function App() {
   setBooks(updatedBooks)
   }
  
-  // // //add function to change Favourite of book(favorite: true or false)
-  // const favoriteStatus = (id) => {
-  //   let updatedBooks = books.map(book => {
-  //     return book.id === id ? {...book, favorite: !book.favorite} : book
-  //   })
-  //  setBooks(updatedBooks)
-  //  }
-   
-
+  // //add function to change Favourite of book(favorite: true or false)
+  const favoriteStatus = (id) => {
+    let updatedBooks = books.map(book => {
+      return book.id === id ? {...book, favorite: !book.favorite} : book
+    })
+   setBooks(updatedBooks)
+   }
   
 
-   useEffect(() => {
+  useEffect(() => {
      const getBooksData = async () => {
        let response = await fetch('http://openlibrary.org/subjects/art.json');
        let data = await response.json();
@@ -53,44 +49,75 @@ function App() {
        }));
       // concat the transformed api books, to the local books, and update the origina book list
       let allBooksCombined = booksData.concat(...apiBooksData);
-      setBooks(allBooksCombined);
+      setBooks(allBooksCombined)
      };
-     getBooksData();
-   }, []);
+     getBooksData()
+    }, []);
+
+  useEffect(() => {
+      const calculateFavSum = () => {
+        const favSum = books.filter(item => item.favorite === true).length;
+        setFavSum(favSum);
+      };
+      calculateFavSum();
+    }, [books]);
+
+  useEffect(() => {
+      const calculateReadSum = () => {
+        const readSum = books.filter(item => item.haveRead === true).length;
+        setReadSum(readSum);
+      };
+      calculateReadSum();
+    }, [books]);
+  
+   const totalBooks = books.length
+  
+   const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }; 
+  
+
+
 
   return (
     <>
         
       <div className="p-5 text-center bg-body-tertiary" >
-        <h2 className="mb-6"><FontAwesomeIcon icon={faBookOpenReader} /></h2>
-        <h1 className="mb-3">MY BOOK LIBRUARY APP</h1> 
-        <div className="container">
-           <div className="row">
-             {/* <div className="col-sm">
-             <FontAwesomeIcon icon={faBookmark} style={{color: "#0096ff",}}/> Read Books {haveReadSum + apiHaveReadSum}
+         <h2 className="mb-6"><FontAwesomeIcon icon={faBookOpenReader} /></h2>
+         <h1 className="mb-3">MY BOOK LIBRUARY APP</h1> 
+          <div className="container">
+              <div className="row">
+                <div className="col-sm">
+                  <FontAwesomeIcon icon={faBookmark} style={{color: "#0096ff",}}/> Read Books {readSum}
+                </div>
+                <div className="col-sm">
+                  Total Books {totalBooks}
+                </div>
+                <div className="col-sm">
+                  <FontAwesomeIcon icon={faHeart} style={{color: "#ff89d8",}}/> Favorite Books {favSum}
+                </div>
+                <hr className="border-top border-secondary"style={{ marginTop: '30px' }} /> 
              </div>
-           <div className="col-sm">
-             Total Books {totalBooks}
-              </div>
-           <div className="col-sm">
-             <FontAwesomeIcon icon={faHeart} style={{color: "#ff89d8",}}/> Favorite Books {favSum + apiFavSum}
-           </div> */}
-         </div>
-      </div>
-
-        <hr className="border-top border-secondary"style={{ marginTop: '30px' }} /> 
-        
-
-      </div>
-      
+          </div>
+      </div>       
       <div className="text-bg-light p-3">
         <div className="collection-list">
-            <h3 className="text-center "style={{ paddingBottom: '60px' }}>ALL BOOKS </h3> 
-            {books.map(book => (<BookCard book={book} toggleStatus={toggleReadStatus}  key = {book.id}></BookCard>  ))}
-            {/* {apiBooks.map(apiBook => (<BookCard book={apiBook} toggleStatus={apiToggleReadStatus} key = {apiBook.id}></BookCard>  ))} */}
+            <h3 className="text-center "style={{ paddingBottom: '60px' }}>ALL BOOKS</h3> 
+            {books.map(book => (<BookCard book={book} toggleStatus={toggleReadStatus} favoriteStatus={favoriteStatus}  key = {book.id}></BookCard>  ))}
+            {books.map(book => (<BookCard book={book} toggleStatus={toggleReadStatus} favoriteStatus={favoriteStatus}  key = {book.id}></BookCard>  ))} 
         </div>
-        <hr className="border-top border-secondary"style={{ marginTop: '30px' }} /> 
+        <button type="button" className="btn btn-danger btn-floating btn-lg" id="btn-back-to-top" onClick={scrollToTop} style={{ float: 'right' }}>
+         <FontAwesomeIcon icon={faArrowUp} />
+        </button>
+        <hr className="border-top border-secondary"style={{ marginTop: '50px' }} /> 
       </div>
+
+
+
+   
       
       
     </>
--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -6,33 +6,36 @@ export default function BookCard({ book, toggleStatus, favoriteStatus }) {
 
   return (
     <div style={{ maxWidth: '700px' }} className="container card mb-3">
-      <div className="row g-0">
-        <div className="col-md-4 d-flex align-items-center">
+    <div className="row g-0">
+      <div className="col-md-4 d-flex align-items-center">
         <img src={book.coverImage} alt={book.id} style={{ maxWidth: '80%' }} />
+      </div>
 
-        <div className="col-md-8">
-          <div className="card-body px-2">
-            <h3 className="card-title">{book.title}</h3>
-            <h5>{book.authors}</h5>
-            <p>{book.year}</p>
-            <p className="card-text">{book.description}</p>
-
-            <div className="container">
-              <div className="row">
-                <div className="d-flex align-items-center">
-                    <FontAwesomeIcon icon={book.haveRead ? faBookmark : faBookmark } style={{color: book.haveRead ? "#0096ff" : '#d5d5d5'}} />   
-                    <h5><Badge pill bg="light" text="dark" className="statusButton" onClick={() => {toggleStatus(book.id)}}  > {book.haveRead ? 'Have read it' : 'Want to read it'}
-                    </Badge> </h5>
-                </div>
-            
+      <div className="col-md-8">
+        <div className="card-body px-2">
+          <h3 className="card-title">{book.title}</h3>
+          <h5>{book.authors}</h5>
+          <p>{book.year}</p>
+          <p className="card-text">{book.description}</p>
+           <div className="container">
+            <div className="row">
+               <div className="d-flex align-items-center">
+                <FontAwesomeIcon icon={book.haveRead ? faBookmark : faBookmark } style={{color: book.haveRead ? "#0096ff" : '#d5d5d5'}} />   
+                <h5><Badge pill bg="light" text="dark" className="statusButton" onClick={() => {toggleStatus(book.id)}}  > {book.haveRead ? 'Have read it' : 'Want to read it'}
+                </Badge> </h5>
+               </div>
+              <div className="d-flex align-items-center">
+                <FontAwesomeIcon icon = {book.favorite? faHeart  : faHeartCrack} style={{ color: book.favorite ? '#ff89d8' : '#d5d5d5' }} />
+                <h5><Badge pill bg="light" text="dark" className="statusButton" onClick={() => {favoriteStatus(book.id)}} > {book.favorite ? 'Favorite' : 'Unfavorite'}
+                </Badge> </h5>
               </div>
             </div>
+          </div>
             <div className="w-100"></div>
             <div className="card-footer w-100 text-muted">{book.memo}</div>
-          </div>
         </div>
       </div>
     </div>
     </div>
-  );
+);
 }
